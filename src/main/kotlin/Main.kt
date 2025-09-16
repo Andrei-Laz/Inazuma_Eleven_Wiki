56d0cc20c8c35251b0d77e580f83084022bffc3b
Andrei-Laz <andlaz2@alu.edu.gva.es>
2025-09-16 16:53:50 +0200

    Version implementing a placeholder (in the form of a hiiden file: .gitkeep) in the empty Hissatsu folder so that it gets saved by the git commands which oterwise would not save it

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,6 +9,8 @@ fun main() {
 
     val ficheroNombres = Path.of("Players/Player_Names.txt")
 
+    val placeholder = Path.of("Hissatsus/.gitkeep")
+
     try {
         if (Files.notExists(carpetaTecnicas)) {
             Files.createDirectories(carpetaTecnicas)
@@ -17,17 +19,15 @@ fun main() {
             Files.createDirectories(carpetaPersonajes)
         }
 
-        Files.list(carpetaPersonajes).use {stream ->
-            stream.sorted().forEach {item ->
-
-            }
-        }
-
         if (Files.notExists(ficheroNombres)) {
             println("No se ha encontrado el fichero con el nombre: " + ficheroNombres.fileName.toString())
         }
         else {
             println("Encontrado el fichero con el nombre: " + ficheroNombres.fileName)
+        }
+
+        if (Files.notExists(placeholder)) {
+            Files.createFile(placeholder)
         }
 
     } catch (e: Exception) {
